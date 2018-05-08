a06f31bd9dae357342f1db802a71d56db73cc8dc
HOME <olga81koppel@gmail.com>
2018-05-08 16:38:07 +0300

    Мультипоточность Improvement 2

--- src/main/java/BrowserManager.java
+++ src/main/java/BrowserManager.java
@@ -12,7 +12,7 @@ public class BrowserManager {
     }
 
     public static void setBrowser() {
-        System.setProperty("webdriver.chrome.driver", "c:/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "./target/classes/chromedriver.exe");
         browser.set(new ChromeDriver());
         getBrowser().manage().window().maximize();
         getBrowser().manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
--- src/test/java/PgFcFacebookLoginTest.java
+++ src/test/java/PgFcFacebookLoginTest.java
@@ -7,12 +7,11 @@ public class PgFcFacebookLoginTest {
 
     @BeforeMethod
     public void browser() {
-        BrowserManager browserManager = new BrowserManager();
-        browserManager.setBrowser();
+        BrowserManager.setBrowser();
     }
 
-    public void loginFacebook(String LOGIN, String PASSWORD) {
-        FeedPage feedPage = Navigation.openMainPage().loginFacebook(LOGIN, PASSWORD);
+    public void loginFacebook(String login, String password) {
+        FeedPage feedPage = Navigation.openMainPage().loginFacebook(login, password);
         Assert.assertTrue(feedPage.isUserIconDisplayed(), "The icon of the user should be present");
     }
 
